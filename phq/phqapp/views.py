a531fc828f83f1b413f5c293c022291258c52dc8
AkashJavali001 <akashjavali297@gmail.com>
2024-11-01 09:20:55 +0530

    ok

--- phq/phqapp/views.py
+++ phq/phqapp/views.py
@@ -3,7 +3,6 @@ import os
 from django.shortcuts import render
 from .forms import PHQ9Form
 
-# Load the model when the server starts
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'ml_models/depression_model.pkl')
 with open(MODEL_PATH, 'rb') as file:
     model = pickle.load(file)
@@ -13,13 +12,13 @@ def predict_depression(request):
     if request.method == 'POST':
         form = PHQ9Form(request.POST)
         if form.is_valid():
-            # Prepare data for prediction
+
             input_data = [[
                 form.cleaned_data['phq1'], form.cleaned_data['phq2'], form.cleaned_data['phq3'],
                 form.cleaned_data['phq4'], form.cleaned_data['phq5'], form.cleaned_data['phq6'],
                 form.cleaned_data['phq7'], form.cleaned_data['phq8'], form.cleaned_data['phq9']
             ]]
-            # Make prediction
+
             prediction = model.predict(input_data)[0]
     else:
         form = PHQ9Form()
